fc736df9be508c876f57ec1ae799b7c4f48135eb
Camassandra <pontecamilla@yahoo.com>
2017-02-10 12:08:37 +0100

    sorting out the app

--- app.js
+++ app.js
@@ -1,43 +1,85 @@
 const express = require('express');
-const fs = require('fs');
-
 const app = express();
+
+const fs = require('fs')
+
+var bodyParser = require('body-parser')
+
+var readjson = require(__dirname+"/search-module");
+
+//here comes the middleware stuff!!!!!!!!
+app.use(express.static(__dirname+'/views/includes')) //a programming line to serve from a folder named 'static' 
+//containinbg static files
+//bodyparser here
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'pug');
 
 //route 1
-app.get('/', function(req, response){
-	fs.readFile('./users.json', 'utf-8', function(err, data){
-		console.log('about to render users page')
-		if (err){
-			throw err;
-		}
-		const fullusers =JSON.parse(data);
-		response.render('index', {indexdata: fullusers});
-	});
+app.get('/', function(reqest, response){
+	readjson(__dirname+'/users.json', function(data) {
+		response.render('index', {indexdata: data});	
+	})
+	
+	
 });
 //route 2
-app.get('/search', function(request, response){
+
+app.get('/search', function(reqest, response){
 	console.log('displaying search bar');
 	response.render('search');
 });
 
+app.post('/searchhandler', function(request, response){
+	var searchquery=request.body.searchquery
+	var searchqueryLower = searchquery.toLowerCase()
+	readjson(__dirname+ '/users.json', function(data){ //data is an array
+		for (var i = 0; i < data.length; i++) {
+			var lowerCaseFirstname = data[i].firstname.toLowerCase()
+			var lowerCaseLastname = data[i].lastname.toLowerCase()
+			console.log(lowerCaseFirstname)
+			if (searchqueryLower === lowerCaseFirstname || searchqueryLower===lowerCaseLastname){
+				response.render('searchresult', {userfound: data[i]})
+				console.log({userfound:data[i]})
+			}
+			//else {
+				//response.redirect('/')
+				//response.status(404).json({message: "User not found"})
+			//}
+		}
+	})
+})
 
 
-//route 3 search for jessicaml on git 
-//app.use(bodyParser.json());
 
-app.post("/", function (req, res) {
-	if (userinput === indexdata.)
-    console.log(req.body.user.name)
-});
-
-//route 4
-app.get('/newuserform', function(request, response){
-	console.log('render form page');
+//route 4 
+// JSON.stringify, FS writefile, fs readfile 
+app.get('/register', function(request, response){
 	response.render('form')
 })
+
+app.post("/register", function(request, response) {
+	readjson(__dirname+'/users.json', function(data){
+		var newUser = request.body
+		data.push(newUser) //returns number of elements in the new array
+		var newuserlist = data //must store the new json file in a var
+		//console.log(newuserlist)
+		 var stringifiedlist = JSON.stringify(newuserlist) 
+		fs.writeFile(__dirname+'/users.json', stringifiedlist, (err) => {
+			if (err) throw err;
+			console.log('saved!');
+		});
+
+	})
+	response.redirect('/')
+})
+
+
+// let newuser = require ('fullusers');
+
 
 app.listen(3000, function(){
 	console.log('listening has started');
---
+++ search-module.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+
+
+const AllUsers = function (filename, callback){
+		fs.readFile(filename, 'utf-8', function(err, data){
+		console.log('parsing users.json')
+		if (err){
+			throw err;
+		}
+		const fullusers =JSON.parse(data);
+		callback(fullusers);
+		});
+	};
+
+
+function searchFirstName(input, user) {
+  return user.firstname.toLowerCase().includes(input.toLowerCase());
+}
+
+function searchLastName(input, user) {
+  return user.lastname.toLowerCase().includes(input.toLowerCase());
+}
+
+module.exports= AllUsers;
